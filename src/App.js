5ce7f6f34de6ced99c60a6cb3561a365dc1e7cd2
kamiya <kamiya@kamiyatoyoakinoMac-mini.local>
2025-07-05 12:13:53 +0900

    更新

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import IntroPage from "./IntroPage";
 import StorePage from "./StorePage";
 import SliderPage from "./SliderPage";
 import MapPage from "./MapPage";
+import ProductPage from "./ProductPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/store" element={<StorePage />} />
       <Route path="/slider" element={<SliderPage />} />
       <Route path="/map" element={<MapPage />} />
+      <Route path="/products/:jan" element={<ProductPage />} />
     </Routes>
   );
 }
