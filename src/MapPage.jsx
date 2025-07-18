e4db8aac035bfba05c2ab18fb90b703d6a0be937
kamiya <kamiya@kamiyatoyoakinoMac-mini.local>
2025-07-18 13:43:01 +0900

    更新

--- src/MapPage.jsx
+++ src/MapPage.jsx
@@ -629,7 +629,7 @@ function App() {
       setViewState((prev) => ({
       ...prev,
       target: [coords[0], coords[1]+5.0, 0],
-      zoom: 8.0  // ← ズーム指定
+      zoom: 4.5  // ← ズーム指定
     }));
 
       const nearest = data
