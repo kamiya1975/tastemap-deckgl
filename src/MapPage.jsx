006174983b36520b468252fd0ab5e524b7fbb3ab
kamiya <kamiya@kamiyatoyoakinoMac-mini.local>
2025-07-26 14:33:30 +0900

    更新

--- src/MapPage.jsx
+++ src/MapPage.jsx
@@ -497,7 +497,7 @@ const ratingCircleLayers = useMemo(() => {
           }),
           mainLayer,
           sliderMarkLayer,
-          //ratingDateLayer,
+          ratingDateLayer,
           textLayer,
           pinLayer,
         ]}
